b2e34fba27c2abc754f0075bd8e3c382b60f438c
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-03 17:39:12 -0500

    created woodcutting class and log, oak, and willow calculations

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-void woodcutting() {
-    std::cout << "woodcutting!";
-}
+class Woodcutting {
+private:
+    const double LOG{25};
+    const double OAK_LOG{37.5};
+    const double WILLOW_LOG{67.5};
+public:
+    void calculate(int xp) {
+        int logs_needed = static_cast<int>(std::ceil(xp / LOG));
+        int oak_logs_needed = static_cast<int>(std::ceil(xp / OAK_LOG));
+        int willow_logs_needed = static_cast<int>(std::ceil(xp / WILLOW_LOG));
+        std::cout <<
+            "You would need:\n"
+            "\t" << logs_needed << " more logs\n"
+            "\t" << oak_logs_needed << " more oak logs\n"
+            "\t" << willow_logs_needed << " more willow logs\n"
+            "to level up. Happy grinding!\n";
+    }
+};
 
-void firemaking() {
+void firemaking(int xp) {
     std::cout << "firemaking!";
 }
 
@@ -19,16 +34,21 @@ int main() {
             "\t[1] Woodcutting\n"
             "\t[2] Firemaking\n"
             "\t(more skills coming soon!)\n";
-
     int skill_choice{};
     std::cin >> skill_choice;
 
+    std::cout << "How much more XP do you need to get to the next level?\n";
+    int more_xp{};
+    std::cin >> more_xp;
+
     switch (skill_choice) {
-        case 1:
-            woodcutting();
+        case 1: {
+            Woodcutting skill;
+            skill.calculate(more_xp);
             break;
+        }
         case 2:
-            firemaking();
+            firemaking(more_xp);
             break;
         default:
             std::cout << "Not a valid skill choice!\n";
